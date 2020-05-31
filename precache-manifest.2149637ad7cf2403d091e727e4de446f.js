6d4070ea95624ec19655a331fe3fd417448a3d5d
Diego Sánchez <diegosanchezrugeles@gmail.com>
2020-05-31 11:42:20 -0500

    Updates

---
+++ precache-manifest.2149637ad7cf2403d091e727e4de446f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c598bc4bdaf7c4caa5ac4ca63578d5f",
+    "url": "/Rateo/index.html"
+  },
+  {
+    "revision": "7f66799ecd081f0d22df",
+    "url": "/Rateo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ba18f692bd089e4b1aa8",
+    "url": "/Rateo/static/css/main.13ce911d.chunk.css"
+  },
+  {
+    "revision": "7f66799ecd081f0d22df",
+    "url": "/Rateo/static/js/2.bc692c23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Rateo/static/js/2.bc692c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba18f692bd089e4b1aa8",
+    "url": "/Rateo/static/js/main.49a0ad2f.chunk.js"
+  },
+  {
+    "revision": "72e83999c46d89f6ab78",
+    "url": "/Rateo/static/js/runtime-main.c0eb31b4.js"
+  }
+]);
